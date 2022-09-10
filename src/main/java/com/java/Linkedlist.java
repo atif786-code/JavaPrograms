d5369e17b734d6b13b7778d30b045588c006d266
atif786-code <cs_18javedkhan@bbdu.ac.in>
2022-09-11 00:50:58 +0530

    updated code

---
+++ src/main/java/com/java/Linkedlist.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.LinkedList;
+
+public class Linkedlist {
+	public static void main(String[] args) {
+		System.out.println("I am goung to start linkedlist ");
+		LinkedList<String> lk = new LinkedList<>();
+		lk.add("One");
+		lk.add("Two");
+		lk.add("three");
+		System.out.println(lk);
+		
+	}
+
+}
